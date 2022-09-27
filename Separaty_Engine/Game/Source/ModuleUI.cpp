40a7763c27603754f215befe0319bfd8495b63b7
Gerard Martinez <gerardmartinez2002@gmail.com>
2022-09-27 02:44:20 +0200

    Module UI

---
+++ Separaty_Engine/Game/Source/ModuleUI.cpp
@@ -0,0 +1,47 @@
+#include "Globals.h"
+#include "Application.h"
+#include "ModuleUI.h"
+
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+//#include "opengl_shaders.h"
+
+
+
+
+ModuleUI::ModuleUI(bool start_enabled) : Module(start_enabled)
+{
+
+}
+
+ModuleUI::~ModuleUI()
+{}
+
+
+bool ModuleUI::Start()
+{
+	
+}
+
+
+bool ModuleUI::CleanUp()
+{
+}
+
+// Update
+update_status ModuleUI::Update(float dt)
+{
+
+	
+
+	return UPDATE_CONTINUE;
+}
+
+update_status ModuleUI::PostUpdate(float dt)
+{
+	
+
+	return UPDATE_CONTINUE;
+}
+
---
+++ Separaty_Engine/Game/Source/ModuleUI.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "Module.h"
+#include "Globals.h"
+
+class ModuleUI : public Module
+{
+public:
+	ModuleUI(bool start_enabled = true);
+	~ModuleUI();
+
+	bool Start();
+	update_status Update(float dt) override;
+	update_status PostUpdate(float dt) override;
+	bool CleanUp();
+
+	//TODO 9: Create an "OnCollision" method specific for this module
+
+
+
+private:
+	
+};
